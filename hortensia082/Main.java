b6cb10953f0bc5819e309f8566da86ef913f1fe0
dongh <dongh@DESKTOP-JCTGHIR>
2020-06-13 02:41:46 +0900

    개인 폴더 만들어 놓기 끝

---
+++ hortensia082/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("코드만 저장해서 보는 곳!! ");
+		System.out.println("실행하려면 소스폴더에 만들어서 해야해요~");
+		System.out.println("");
+	}
+
+}
