638cb0c8ada8d7697de1c6ba22a493ea913112e8
bit <bit@DESKTOP-P3LKJ9U>
2020-06-15 14:07:23 +0900

    0615

--- 세리/Main.java
+++ 세리/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("코드만 저장해서 보는 곳!! ");
 		System.out.println("실행하려면 소스폴더에 만들어서 해야해요~");
-		System.out.println("");
+		System.out.println("  ");
 	}
 
 }
