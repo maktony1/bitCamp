ed7e40032a839a859272f401d911f800364c91d7
bit <bit@DESKTOP-T48RHNQ>
2020-06-24 14:31:34 +0900

    member

---
+++ 동휘/JSP/0624멤버 DAO DTO/MemberDAO.java
@@ -0,0 +1,67 @@
+package edu.bit.ex.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import edu.bit.ex.dto.MemberDTO;
+
+public class MemberDAO {
+	private String url = "jdbc:oracle:thin:@localhost:1521:xe";
+	private String uid = "scott";
+	private String upw = "tiger";
+	private String driver= "oracle.jdbc.driver.OracleDriver";
+	
+	public MemberDAO() {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public ArrayList<MemberDTO> memberSelect(){
+		ArrayList<MemberDTO> dtos = new ArrayList<MemberDTO>();
+		Connection con = null;
+		Statement stmt = null;
+		ResultSet rs   = null;
+		String sql = "select * from member";
+		try {
+			con = DriverManager.getConnection(url,uid,upw);
+			stmt = con.createStatement();
+			rs = stmt.executeQuery(sql);
+			
+			while(rs.next()) {
+				String name = rs.getString("name");
+				String id = rs.getString("id");
+				String pw = rs.getString("pw");
+				String phone1 = rs.getString("phone1");
+				String phone2 = rs.getString("phone2");
+				String phone3 = rs.getString("phone3");
+				String gender = rs.getString("gender");
+				
+				MemberDTO dto = new MemberDTO(name, id, pw, phone1, phone2, phone3, gender);
+				dtos.add(dto);
+			}
+			
+			
+			
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(rs != null) rs.close();
+				if(stmt != null) stmt.close();
+				if(con != null) con.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return dtos;
+		
+	}
+}
